6bbcd7481d9995fcfdf929f8cdd3b30de523761f
siveozdemir6 <siveozdemir6@gmail.com>
2025-06-14 00:56:35 +0300

    Create SQL_Odev11.sql

---
+++ SQL_Odev11.sql
@@ -0,0 +1,32 @@
+SELECT first_name FROM actor
+UNION 
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
+
+--İlk üç sorguyu tekrar eden veriler için de yapıyoruz.
+
+SELECT first_name FROM actor
+UNION ALL
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+INTERSECT ALL
+SELECT first_name FROM customer;
+
+SELECT first_name FROM actor
+EXCEPT ALL
+SELECT first_name FROM customer;
+
+
+
+
+
+
+
